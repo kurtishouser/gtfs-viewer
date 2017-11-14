14310f7c538bbb4b5e55cdbd4889ae37c920fc04
Kurtis Houser <houserkurtis@gmail.com>
2017-11-14 15:30:36 -0800

    Remove unneeded files

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Header } from 'semantic-ui-react';
 
-// import Services from './Services';
 import Shapes from './Shapes';
 
 export const Content = () => (
@@ -10,7 +9,6 @@ export const Content = () => (
       Route Viewer
     </Header>
     <div>
-      {/* <Services /> */}
       <Shapes />
     </div>
   </Container>
