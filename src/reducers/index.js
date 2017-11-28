bc042aad52a412f05853b2a7f3480713680906ed
Kurtis Houser <houserkurtis@gmail.com>
2017-11-28 14:04:50 -0800

    Add settings reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
+import settings from './settings';
 import agencies from './agencies';
 import routes from './routes';
 import shapes from './shapes';
 
 export default combineReducers({
+  settings,
   agencies,
   routes,
   shapes,
---
+++ src/reducers/settings.js
@@ -0,0 +1,11 @@
+function settings(state = {
+  routeFilters: {
+    type: 'all',
+    direction: 'all',
+    service: 'all',
+  },
+}) {
+  return state;
+}
+
+export default settings;
