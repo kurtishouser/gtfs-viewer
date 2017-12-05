15cfb295378195239d566c24e65916cb9eda6dbe
Kurtis Houser <houserkurtis@gmail.com>
2017-12-05 15:42:37 -0800

    Comment out Redux DevTools Extension for deployment build

--- src/store.js
+++ src/store.js
@@ -3,15 +3,9 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import Api from './utils/Api';
 
-/* eslint-disable no-underscore-dangle */
-// const store = createStore(
-//   rootReducer,
-//   applyMiddleware(thunkMiddleware.withExtraArgument({ Api })),
-// );
-
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunkMiddleware.withExtraArgument({ Api })),
 );
 
